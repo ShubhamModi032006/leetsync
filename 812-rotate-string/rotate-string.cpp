7088808ab4f33a5c28efafef696674b64984d5ad
shubham modi <shubham.modi.cg@gmail.com>
2026-01-12 20:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.10%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
-        int n = s.size();
 
-        for (int idx = 0; idx < n; idx++) {
-            if (goal[idx] == s[0]) {
-                bool match = true;
-                for (int i = 0; i < n; i++) {
-                    if (s[i] != goal[(i + idx) % n]) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) return true;
-            }
+        for (int i = 0; i < s.size(); i++) {
+            rotate(begin(s), begin(s)+1, end(s));
+            if(s == goal) return true;
         }
         return false;
     }
 };
+
