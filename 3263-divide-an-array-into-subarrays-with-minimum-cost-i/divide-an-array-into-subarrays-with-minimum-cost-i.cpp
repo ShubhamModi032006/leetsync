5277721c1a9c3b36c87c1f0dc837a25faa9b8a0e
shubham modi <shubham.modi.cg@gmail.com>
2026-02-01 22:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (17.09%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        int a = 51;
-        int b = 51;
-        for(int i = 1;i<nums.size();i++){
-            b = min(b, nums[i]);
-            if(a > b) swap(a,b);
-            if (a == 1 && b == 1) break;            
+        // int a = 51;
+        // int b = 51;
+        // for(int i = 1;i<nums.size();i++){
+        //     b = min(b, nums[i]);
+        //     if(a > b) swap(a,b);
+        //     if (a == 1 && b == 1) break;            
+        // }
+        // return nums[0] + a + b;
+
+
+        // sort(nums.begin()+1,nums.end());
+        // int res = nums[0];
+        // return res + nums[1] + nums[2];
+
+        int ans = INT_MAX;
+        for(int i=1;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                ans = min(ans, nums[0] + nums[i] + nums[j]);
+            }
         }
-        return nums[0] + a + b;
+        return ans;
     }
 };
