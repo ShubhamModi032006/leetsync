d2d4d16f70f257516007b50375520aa2599dfd28
shubham modi <shubham.modi.cg@gmail.com>
2026-02-02 10:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (53.54%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     int longestMountain(vector<int>& arr) {
+        // int n = arr.size();
+        // if (n < 3)
+        //     return 0;
+        // int maxl = 0;
+        // int len = 0;
+        // for (int i = 1; i < n; i++) {
+        //     int up = 0;
+        //     int down = 0;
+        //     int j = i;
+        //     while (j < n && arr[j] > arr[j - 1]) {
+        //         j++;
+        //         up++;
+        //     }
+
+        //     while (up != 0 && j < (n) && arr[j] < arr[j - 1]) {
+        //         j++;
+        //         down++;
+        //     }
+        //     if (up > 0 && down > 0) {
+        //         maxl = max(maxl, up + down + 1);
+        //     }
+        // }
+        // return maxl;
+
+        int maxl = 0;
         int n = arr.size();
         if (n < 3)
             return 0;
-        int maxl = 0;
-        int len = 0;
-        for (int i = 1; i < n; i++) {
-            int up = 0;
-            int down = 0;
+        for (int i = 1; i < n;) {
+            if (arr[i] <= arr[i - 1]) {
+                i++;
+                continue;
+            }
             int j = i;
-            while (j < n && arr[j] > arr[j - 1]) {
-                j++;
-                up++;
-            }
-
-            while (up != 0 && j < (n) && arr[j] < arr[j - 1]) {
-                j++;
-                down++;
-            }
-            if (up > 0 && down > 0) {
+            int up = 0;
+            bool f1 = false;
+            int down = 0;
+            bool f2 = false;
+            while (j < n && arr[j] > arr[j - 1])
+                j++, up++, f1 = true;
+            while (j < n && arr[j] < arr[j - 1])
+                j++, down++, f2 = true;
+            if (up + down + 1 > 2 && f1 && f2)
                 maxl = max(maxl, up + down + 1);
-            }
+            i = j;
         }
         return maxl;
     }
